421ef3b7e5367c9a2c0ad9ebf7f5f2f2b1bbf557
hellowebtest <rivalu4@gmail.com>
2019-12-09 23:54:31 +0200

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,5 +1,6 @@
---some query
+--some
+--query
 SELECT OrderNumber, TotalAmount, FirstName, LastName, City, Country
   FROM Customer C LEFT JOIN [Order] O
-    ON O.CustomerId = C.Id --comment
+    ON O.CustomerId = C.Id --comment new
 ORDER BY TotalAmount
