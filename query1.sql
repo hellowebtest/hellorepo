7343006e693efcf296292b5ea587ce8fb9fd71f7
Taras Pankiv <teenu4k@gmail.com>
2019-12-14 12:32:40 +0200

    new commit

--- query1.sql
+++ query1.sql
@@ -1,4 +1,4 @@
 SELECT OrderNumber, TotalAmount, FirstName, LastName, City, Country
-  FROM Customer C LEFT JOIN [Order] O --comment
+  FROM Customer C LEFT JOIN [Order] O --comment additional
     ON O.CustomerId = C.Id
 ORDER BY TotalAmount
