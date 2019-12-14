4dccc90c351b9f0b683892599e0b2bc599d49d7c
Taras Pankiv <teenu4k@gmail.com>
2019-12-14 12:42:29 +0200

    multiple

--- query1.sql
+++ query1.sql
@@ -1,4 +1,7 @@
 SELECT OrderNumber, TotalAmount, FirstName, LastName, City, Country
+--this added
+--this too
+--this too[2]
   FROM Customer C LEFT JOIN [Order] O --comment additional
-    ON O.CustomerId = C.Id
+    ON O.CustomerId = C.Id-- this changed
 ORDER BY TotalAmount
